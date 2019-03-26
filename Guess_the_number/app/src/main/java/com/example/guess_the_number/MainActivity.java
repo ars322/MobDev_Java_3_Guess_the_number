50575456c3c83816d68e2f03d84f137805e30a87
mvkoroteev <mvkoroteev@fa.ru>
2019-03-26 17:50:41 +0300

    Variables created

--- Guess_the_number/app/src/main/java/com/example/guess_the_number/MainActivity.java
+++ Guess_the_number/app/src/main/java/com/example/guess_the_number/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.guess_the_number;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvInfo;
+    EditText etInput;
+    Button bControl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvInfo = findViewById(R.id.textView);
+        etInput = findViewById(R.id.editText);
+        bControl = findViewById(R.id.button);
+
     }
 }
