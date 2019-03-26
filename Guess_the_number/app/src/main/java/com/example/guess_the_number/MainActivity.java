638192f699d4a9c325f9d6ac706fc4581ca26f9b
mvkoroteev <mvkoroteev@fa.ru>
2019-03-26 17:39:42 +0300

    Initial commit

---
+++ Guess_the_number/app/src/main/java/com/example/guess_the_number/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.guess_the_number;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
